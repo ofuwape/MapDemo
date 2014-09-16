9a98f2c8a2c9c338430dc7ac58c131cebc0f2006
OLUWATONI FUWAPE <ofuwape@gmail.com>
2014-09-16 12:25:33 -0400

    Initial Commit

---
+++ MapDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapDemo
+//
+//  Created by Maputi Botlhole on 9/16/14.
+//  Copyright (c) 2014 Oluwatoni Fuwape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapDemo
+//
+//  Created by Maputi Botlhole on 9/16/14.
+//  Copyright (c) 2014 Oluwatoni Fuwape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
